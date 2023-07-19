a266ee33c3d433a3e0a9355831be551469666db6
Umang Gupta <guptaumang769@gmail.com>
2023-07-19 09:23:15 +0530

    Assignments

---
+++ Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -0,0 +1,27 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-anagram`
+*/
+
+function sort(str) {
+  var array = str.split("");
+  array = array.sort();
+  var sortedString = array.join("");
+  return sortedString;
+}
+
+function isAnagram(str1, str2) {
+  let aLower = str1.toLowerCase();
+  let bLower = str2.toLowerCase();
+  if(sort(aLower) == sort(bLower)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = isAnagram;
