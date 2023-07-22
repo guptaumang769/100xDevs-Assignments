4b07e36edabd73c3a6eb85269d7be3a7539e20b7
Umang Gupta <guptaumang769@gmail.com>
2023-07-22 08:48:55 +0530

    Added logic of palindrome

--- Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -6,7 +6,24 @@
   - `npm run test-palindrome`
 */
 
+function removeSpacesAndPunctuations(str) {
+  var nopunctuation = str.replace(/\W/g,"");
+  var nospaces = nopunctuation.replace(/\s/g,"");
+  var finalstring = nospaces;
+
+  return finalstring;
+}
+
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  let finalstring = removeSpacesAndPunctuations(str);
+
+  let len = finalstring.length;
+  for(let i=0; i<Math.floor(len/2); i++) {
+    if (finalstring[i] !== finalstring[len - 1 - i]) {
+      return false;
+    }
+  }
   return true;
 }
 
